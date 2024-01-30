42867ff9b0677f9612f078b068ef86d0ac3c121e
juraph <local.juraph@gmail.com>
2024-01-31 06:17:53 +1000

    Begin adding fsrs support to org-drill

---
+++ org-drill-fsrs.el
@@ -0,0 +1,25 @@
+;;; org-drill-fsrs.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024
+;;
+;; Author:  <juraph@juraph.com>
+;; Maintainer:  Juraph
+;; Created: January 31, 2024
+;; Modified: January 31, 2024
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/juraph/org-drill
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary: Implementation of the Anki fsrs algorithm for org-drill (https://github.com/open-spaced-repetition/fsrs4anki/wiki/The-Algorithm)
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'org-drill-fsrs)
+;;; org-drill-fsrs.el ends here
--- org-drill.el
+++ org-drill.el
@@ -719,8 +719,10 @@ CMD is bound, or nil if it is not bound to a key."
 (defun org-drill-time-to-inactive-org-timestamp (time)
   "Convert TIME into org-mode timestamp."
   (format-time-string
-   (concat "[" (substring (cdr org-time-stamp-formats)) "]")
-   time))
+   (if (< emacs-major-version 29)
+       (concat "[" (substring (cdr org-time-stamp-formats) 1 -1) "]")
+       (org-time-stamp-format t 'no-bracket))
+
 
 (defun org-drill-map-entries (func &optional scope drill-match &rest skip)
   "Like `org-map-entries', but only drill entries are processed."
